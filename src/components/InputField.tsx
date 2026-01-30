db586ee1092a0ec3553a76d9e2739093dbf7fdf1
Daud <85739147+RedDead11@users.noreply.github.com>
2026-01-30 22:49:57 +0500

    Initial Commit

---
+++ src/components/InputField.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './styles.css'
+
+interface Props{
+    todo:string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>
+}
+
+const InputField:  React.FC<Props> = ({todo,setTodo}) => {
+  return ( 
+    <form className="input">
+        <input type="input"
+        value={todo}
+        onChange={
+            (e)=>setTodo(e.target.value)
+        }
+        id="task-input" placeholder=' ' 
+        className='input__box'/>
+
+        <label htmlFor="task-input" className='input__label'>
+            Enter a task
+        </label>
+
+        <button className="input__submit" type='submit'>Go</button>
+    </form>
+  )
+}
+
+export default InputField
